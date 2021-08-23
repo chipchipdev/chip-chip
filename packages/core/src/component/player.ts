a4836189bcc9c5eaf3ff8f2be5e1187196cf9de0
chip.chip.dev <chip.chip.dev@gmail.com>
2021-08-24 00:00:48 +0800

    bootstrap the schema into the core project

--- packages/core/src/component/player.ts
+++ packages/core/src/component/player.ts
@@ -1,1 +1,46 @@
-import { PlayerAbstract } from '@chip-chip/schema';
+import {
+  PlayerAbstract,
+  PlayerAction,
+} from '@chip-chip/schema';
+
+class Player extends PlayerAbstract<PlayerAction> {
+  getPlayer(): {
+    id: string; name: string; chips: number;
+    joined: boolean; folded: boolean; allin: boolean; optioned: boolean;
+    action: PlayerAction
+  } {
+    throw new Error('Method not implemented.');
+  }
+
+  setChips(chips: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  setJoined(joined: boolean) {
+    throw new Error('Method not implemented.');
+  }
+
+  setFolded(folded: boolean) {
+    throw new Error('Method not implemented.');
+  }
+
+  setAllin(folded: boolean) {
+    throw new Error('Method not implemented.');
+  }
+
+  setBet(folded: boolean) {
+    throw new Error('Method not implemented.');
+  }
+
+  setOptioned(folded: boolean) {
+    throw new Error('Method not implemented.');
+  }
+
+  setAction(action: PlayerAction) {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export {
+  Player,
+};
--- packages/schema/src/base/index.ts
+++ packages/schema/src/base/index.ts
@@ -1,5 +1,9 @@
+// abstract classes
 export * from './croupier.abstract';
 export * from './match.abstract';
 export * from './player.abstract';
 export * from './pool.abstract';
 export * from './round.abstract';
+
+// other type files
+export * from './type';
